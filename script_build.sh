3fa4813b53019f90ab8d6c43f19a47aed57b63ec
Pulkit077 <pulkitagarwal2k1@gmail.com>
2024-05-21 07:23:52 +0530

    Revert "Update script_build.sh"
    This reverts commit fab86184e5bc0d1226f30c39b49120e7cfa57fe7.

--- script_build.sh
+++ script_build.sh
@@ -27,14 +27,14 @@ then
 echo -e ${blu}"CCACHE is enabled for this build"${txtrst}
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-export CCACHE_DIR=/home/pulkit/.ccache
+export CCACHE_DIR=/home/ccache/$username
 ccache -M 150G
 fi
 
 if [ "$use_ccache" = "clean" ];
 then
 export CCACHE_EXEC=$(which ccache)
-export CCACHE_DIR=/home/pulkit/.ccache
+export CCACHE_DIR=/home/ccache/$username
 ccache -C
 export USE_CCACHE=1
 ccache -M 150G
